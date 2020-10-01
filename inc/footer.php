33482eccdde954aecb4fd57adae03f988289827f
hywn <jh7qbe@virginia.edu>
2020-10-01 00:17:55 -0400

    rewrote footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,77 +1,14 @@
-<div class="footer-container">
-  <div class="footer-info-section">
-    <div class="row footer-row">
-      <div class="footer-contact-col four columns">
-        <p class="footer">contact us<br /><br />feel free to email us with any questions, comments, or concerns:<br /><a href="mailto:greenstogrounds@gmail.com">greenstogrounds@gmail.com</a></p>
-      </div>
-      <div class="footer-social-media four columns">social media<br /><br />
-        <div class="footer-social-media-logos">
-          <a href="https://instagram.com/greenstogrounds/" target="_blank"><img src="lib/imgs/instagram.png" alt="instagram" class="footer-social-media-img"></a><a href="https://www.facebook.com/greenstogrounds/timeline?ref=page_internal" target="_blank"><img src="lib/imgs/facebook.png" alt="facebook" class="footer-social-media-img"></a>
-        </div>
-      </div>
-      <!--<div class="footer-subscribe four columns">subscribe<br /><br />enter your email below to receive our newsletter</div>-->
-    </div>
-  </div>
-  <div class="footer-jubil-section">
-    <p class="jubil-section">&copy; Katz, O'Neil & Graham | All Rights Reserved | Privacy Policy</p>
-  </div>
+<div id="footer">
+	<p>
+		contact: <a href="mailto:greenstogrounds@gmail.com">greenstogrounds@gmail.com</a>
+	</p>
+	<div id="social-media-icons">
+		<a href="https://instagram.com/greenstogrounds/" target="_blank">
+			<img src="lib/imgs/instagram.png" alt="instagram">
+		</a>
+		<a href="https://www.facebook.com/greenstogrounds/timeline?ref=page_internal" target="_blank">
+			<img src="lib/imgs/facebook.png" alt="facebook">
+		</a>
+	</div>
 </div>
-
-<!--FOOTER DESCRIPTION -->
-  <!--<div id="footwrap" >
-    <div class="container" style="background-color: #7f8c8d; width: 100%;">
-      <div class="row">
-
-        <div class="col-lg-3">
-          <h3>CONTACT</h3>
-        </div>
-
-        <div class="col-lg-3">
-          <h3>Email</h3>
-          <h4>
-            greenstogrounds@gmail.com
-            <br/>
-          </h4>
-        </div>
-
-        <div class="col-lg-3">
-          <h3>Social Media:</h3>
-          <ul>
-            <li>
-              <a href="https://www.twitter.com/greenstogrounds"><i class="fa fa-twitter fa-2x"></i></a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/greenstogrounds"><i class="fa fa-facebook fa-2x"></i></a>
-            </li>
-            <li>
-              <a href="http://instagram.com/greenstogrounds"><i class="fa fa-instagram fa-2x"></i></a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-lg-3">-->
-          <!-- Begin MailChimp Signup Form -->
-          <!--<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-          <style type="text/css">
-          </style>
-          <div id="mc_embed_signup">
-            <form action="//greenstogrounds.us9.list-manage.com/subscribe/post?u=0c3ff215d47337f1b1eb60481&amp;id=fcffc6e6ee" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-              <h4>Subscribe <br/> to our mailing list!</h4>
-              <input type="email" value="" name="EMAIL" class="email see-more" id="mce-EMAIL" placeholder="email address" required>
-              <!-- This is to prevent bot signups-->
-              <!--<div style="position: absolute; left: -5000px; display: inline-block;">
-                <input type="text" name="b_0c3ff215d47337f1b1eb60481_fcffc6e6ee" tabindex="-1" value="">
-              </div>
-              <div class="clear">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button see-more">
-              </div>
-            </form>
-          </div>-->
-          <!--End MailChimp Signup Form
-        </div>-->
-        <!--/.col-lg-3
-      </div>-->
-      <!--/.row
-    </div> -->
-    <!--/.container
-  </div>-->
+<div id="copyright">&copy; Katz, O'Neil & Graham | All Rights Reserved</div>
