798ebbb17f04b0829cfa3696feec543e1a2ce349
hywn <jh7qbe@virginia.edu>
2020-10-01 11:29:28 -0400

    cleaned up head

--- inc/head.php
+++ inc/head.php
@@ -1,41 +1,26 @@
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title><?php echo $title ?> | Greens to Grounds</title>
+<title><?php echo $title ?> | Greens to Grounds</title>
 
-  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-  <meta name="description" content="UVA fresh food delivery, community supported agriculture, CSA, greens to grounds, greens, grounds">
-  <meta name="keywords" content="UVA, University of Virginia, food, organic, fresh, farmers market, delivery, food, fruit, vegetables, local">
-  <meta name="author" content="Austin Petrie">
-  <meta name="revised" content="10/12/2015">
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta name="description" content="UVA fresh food delivery, community supported agriculture, CSA, greens to grounds, greens, grounds">
+<meta name="keywords" content="UVA, University of Virginia, food, organic, fresh, farmers market, delivery, food, fruit, vegetables, local">
+<meta name="author" content="Austin Petrie">
+<meta name="revised" content="10/12/2015">
 
-  <!-- CSS reset -->
-  <link href="lib/css/normalize.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="lib/css/site.css">
+<link rel="stylesheet" type="text/css" href="lib/css/nav.css">
+<link rel="stylesheet" type="text/css" href="lib/css/footer.css">
 
-  <!-- Google Fonts -->
-  <!--For headers -->
-  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Vollkorn">
-  <!--For body text -->
-  <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
+<!-- unused font stuff
 
-  <!-- Icons -->
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <!-- Bootstrap Core CSS -->
-  <!--link href="lib/css/bootstrap.min.css" rel="stylesheet"-->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Vollkorn">
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Nobile">
+<style>
+.section { font-family: Nobile }
+h1, h2, .question { font-family: Vollkorn }
+</style>
 
-  <!-- Custom CSS -->
-  <link href="lib/css/nav.css" rel="stylesheet" type="text/css">
-  <link href="lib/css/footer.css" rel="stylesheet" type="text/css">
-  <link href="lib/css/site.css" rel="stylesheet" type="text/css">
-  <!--link href="lib/css/skeleton.css" rel="stylesheet" type="text/css"-->
-
-  <!--style>
-    body {overflow-x: hidden;}
-  </style-->
-
-
-  <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
+-->
