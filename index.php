0225d75fbc26a22612551f3cc9cc771f16c929b1
hywn <jh7qbe@virginia.edu>
2020-10-01 11:33:36 -0400

    removed newlines

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 				<a href="#our-mission"><img src="lib/imgs/arrow.svg" height="60px" width="60px" alt="arrow"></a>
 			</div>
 		</div>
-
 		<div class="section" id="our-mission">
 			<div id="our-mission-body">
 				<div>
@@ -55,9 +54,7 @@
 			<img src="lib/imgs/box.png" alt="handshake">
 			<p style="font-size: 10px; text-align: center; padding: 5px;">Although this organization has members who are University of Virginia students and may have University employees associated or engaged in its activities and affairs, the organization is not a part of or an agency of the University. It is a separate and independent organization which is responsible for and manages its own activities and affairs. The University does not direct, supervise or control the organization and is not responsible for the organization's contracts, acts or omissions.</p>
 		</div>
-
 	</div>
 	<?php include ('inc/footer.php'); ?>
 </body>
-
 </html>
